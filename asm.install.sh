f1b09b262d572a9778d4fa58710dd41f3535cdcb
Virtual Items <virtualitemsuniverse@gmail.com>
2026-09-24 03:28:40 -0500

    Add ufw to package installation in asm.install.sh

--- asm.install.sh
+++ asm.install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apk add binutils build-base elfutils file gdb git linux-headers nasm pax-utils perf strace tmux valgrind vim
+apk add binutils build-base elfutils file gdb git linux-headers nasm pax-utils perf strace tmux ufw valgrind vim
+
+# openssh-client
+# openssh-server
